80227ab5f1797348a4c6f818b9350759e4fd007f
james chalmers <james.chalmers184@gmail.com>
2021-11-08 10:14:27 +0000

    Add project files.

---
+++ MoneyTransferApp/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MoneyTransferApp.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double CurrentBalance { get; set; }
+    }
+}
